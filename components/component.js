13dff736b6fc927b033c496344677d067bfd6d57
root404-ir <mohammadrg360@gmail.com>
2025-01-28 01:13:57 +0330

    add select language

---
+++ components/component.js
@@ -0,0 +1,11 @@
+const googleDesLang = {
+    reply_markup: {
+        inline_keyboard: [
+            [
+                { text: '(🇬🇧) انگلیسی', callback_data: "en" },
+                { text: '(🇮🇷) فارسی', callback_data: "fa" }
+            ]
+        ]
+    }
+}
+module.exports = { googleDesLang }
