2b2a886cde34372f582679c5e546130d48a9a8ec
root404-ir <mohammadrg360@gmail.com>
2025-01-28 14:42:25 +0330

    handle command && actions

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
+//packages
 const TelegramBot = require('node-telegram-bot-api')
+const redis = require('redis')
+
+//config
+const client = redis.createClient()
+client.connect()
 const token = '7404861031:AAEUkfJF28n9NyodeBPr7N15R3UCpef7h4M'
-const actions = require('./actions/actions').default
+
+//utils
+const actions = require('./actions/actions')
+const component = require('./components/component')
+const messages = require('./utils/messages')
 
 
 const bot = new TelegramBot(token, { polling: true })
 //bot start command
 bot.onText(/\/start/, (msg) => actions.homeMenu(bot, msg.chat.id))
+
+bot.on('callback_query', (query) => {
+    const command = query.data
+    const chatId = query.message.chat.id
+    const messageId = query.message.message_id
+
+    if (command === 'google') {
+        actions.sendTranslateKeyboard(bot, chatId, 'action', command, component.googleDesLang, messages.list.selectLanguageText, messageId)
+    }
+    if (command === 'microsoft') {
+        client.set(`user:${chatId}:action`, command)
+        const keyboard = component.microsoftDesLang
+        bot.sendMessage(chatId, list.selectLanguageText, keyboard)
+    }
+    if (command === 'en') {
+        client.set(`user:${chatId}:lang`, command)
+
+    }
+    if (command === 'fa') {
+        client.set(`user:${chatId}:lang`, command)
+
+    }
+})
