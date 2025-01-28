f8868a7dc066645abfdd072ad87a4cbf0f904b27
root404-ir <mohammadrg360@gmail.com>
2025-01-28 14:44:16 +0330

    add microsoftDesLang component

--- components/component.js
+++ components/component.js
@@ -8,4 +8,14 @@ const googleDesLang = {
         ]
     }
 }
-module.exports = { googleDesLang }
+const microsoftDesLang = {
+    reply_markup: {
+        inline_keyboard: [
+            [
+                { text: '(🇬🇧) انگلیسی', callback_data: "en" },
+                { text: '(🇮🇷) فارسی', callback_data: "fa" }
+            ]
+        ]
+    }
+}
+module.exports = { googleDesLang, microsoftDesLang }
