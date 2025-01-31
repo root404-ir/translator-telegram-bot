310d327866d50e097c5d133528064b4f4053e996
root404-ir <mohammadrg360@gmail.com>
2025-01-31 16:56:01 +0330

    minimize and fixed codes

--- index.js
+++ index.js
@@ -18,22 +18,23 @@ bot.onText(/\/start/, (msg) => actions.homeMenu(bot, msg.chat.id))
 
 bot.on('callback_query', (query) => {
     const actionsCommand = ['google', 'microsoft']
+    const languages = ['fa', 'en']
     const command = query.data
     const chatId = query.message.chat.id
     const messageId = query.message.message_id
     const selectedActionsCommand = actionsCommand.find(item => item === command)
-    const list = {
-        selectLanguageText: `موتور ترجمه انتخابی شما : ${selectedActionsCommand} \n\n\n لطفا زبان مقصد را انتخاب کنید`
+    const selectedLanguage = languages.find(lang => lang === command)
+    const listMessages = {
+        selectLanguageText: `موتور ترجمه انتخابی شما : ${selectedActionsCommand} \n\n\n لطفا زبان مقصد را انتخاب کنید`,
+        sendQuery: `زبان مقصد انتخابی شما : ${selectedLanguage} \n\n\n لطفا متنی که میخواهید ترجمه بشه رو ارسال کنید...`
     }
 
-    if (actionsCommand.includes(command)) {
-        actions.sendTranslateKeyboard(bot, chatId, 'action', command, component[`${command}DesLang`], list.selectLanguageText, messageId, actionsCommand)
-    }
-    if (command === 'en') {
-        client.set(`user:${chatId}:lang`, command)
-    }
-    if (command === 'fa') {
-        client.set(`user:${chatId}:lang`, command)
+    actionsCommand.includes(command) ? actions.sendTranslateKeyboard(bot, chatId, 'action', command, component[`${command}DesLang`], listMessages.selectLanguageText, messageId, actionsCommand) : false
 
-    }
+    languages.includes(command) ? actions.sendLanguage(bot, chatId, command, listMessages.sendQuery) : false
+})
+
+bot.on('polling_error', (error) => {
+    console.log(`polling error ${error}`);
+
 })
